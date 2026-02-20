1e429be09bfa3fcc564a4e969cbe43e3148a9a7c
Matheus Henrique <matheus.uranoseguros@gmail.com>
2026-02-19 22:29:50 -0300

    Redirect www to non-www

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.uranoseguros.com.br",
+          },
+        ],
+        destination: "https://uranoseguros.com.br/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
